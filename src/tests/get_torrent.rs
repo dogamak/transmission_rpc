8ab124ddec4dfa2e2d49ecd1945dc1ad46ecc23a
Mitja Karhusaari <monttu300@gmail.com>
2016-12-12 17:33:26 +0200

    Fix test.
    Transmission doesn't intrerpret empty list of requested
    fields as a request to send all field.

--- src/tests/get_torrent.rs
+++ src/tests/get_torrent.rs
@@ -8,18 +8,25 @@ fn get_all() {
 
     // Fetch the values of one specific value for all torrents.
     let req = GetTorrent::new()
-        .field(Field::Id);
+        .field(Field::Id)
+        .field(Field::Name);
 
     let res = tr.send(&req).expect("Error while communicating with the server.");
 
     let first = res.into_iter().next().expect("No torrents returned by the server!");
 
+    assert!(first.id.is_some());
+    assert!(first.name.is_some());
+    
     // Fetch all fields of one specific torrent.
     let req = GetTorrent::new()
-        .id(first.id.unwrap());
+        .id(first.id.unwrap())
+        .field(Field::Name);
 
     let res = tr.send(&req).expect("Error while communicating with the server.");
 
-    assert!(res.into_iter().next().expect("No torrents returned by the server!")
-            .id.expect("No torrent id sent by the server!") != 0);
+    let second = res.into_iter().next().unwrap();
+    
+    assert!(second.name.is_some());
+    assert!(second.name.unwrap() == first.name.unwrap());
 }
